c37d9ad0a660b71dac0f5b53b8d8dd6dea16d56c
Raul Ruiz <rauletxailarena@gmail.com>
2017-10-02 18:43:29 +0100

    Change the require sinatra/contrib/all

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
-require 'sinatra/contrib/all' if development?
+# require 'sinatra/contrib/all' if development?
 require_relative './controllers/eateries_controller'
 require_relative './controllers/burgers_controller'
 require_relative './controllers/deals_controller'
 require_relative './controllers/burger_deals_controller'
 
-also_reload './models/*'
+# also_reload './models/*'
